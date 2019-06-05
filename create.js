37cb462bf32eacc19ed9bfdfaae47dd84f7c2914
savalone47 <savalonescott47@gmail.com>
2019-06-05 19:31:37 +0200

    update

--- create.js
+++ create.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const url = 'mongodb://localhost:27017/test';
 
-mongoose.connect(url,{ useNewUrlParser: true}, function (next) {
+mongoose.connect(url,{ useNewUrlParser: true}, function () {
 	const user1 = new User({firstname: "John", lastname: "Patrick"});
 	user1.save(function(err){
 		if (err) console.log('Error', err);
 		console.log(user1);
 	});
-	next();
 });
 const userSchema = Schema({
 	firstname: String
