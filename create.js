e60c85ebf90a0970ed90dc54de6e3b3303798270
savalone47 <savalonescott47@gmail.com>
2019-06-05 19:13:51 +0200

    create mongo table

---
+++ create.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const url = 'mongodb://localhost:27017/test';
+
+mongoose.connect(url,{ useNewUrlParser: true}, function (next) {
+	const user1 = new User({firstname: "John", lastname: "Patrick"});
+	user1.save(function(err){
+		if (err) console.log('Error', err);
+		console.log(user1);
+	});
+	next();
+});
+const userSchema = Schema({
+	firstname: String
+	, lastname: String
+});
+const User = mongoose.model('User', userSchema);
+
